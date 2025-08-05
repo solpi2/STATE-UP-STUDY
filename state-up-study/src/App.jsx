d7e0ff7c0d093825844265ce977a976808b1b6e5
solpi2 <parkchini@gmail.com>
2025-08-05 15:41:50 +0900

    commit1

--- state-up-study/src/App.jsx
+++ state-up-study/src/App.jsx
@@ -1,7 +1,11 @@
+import A from "./components/A";
+import B from "./components/B";
+
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      App
+      <A />
+      <B />
     </div>
   );
 };
---
+++ state-up-study/src/components/B.jsx
@@ -0,0 +1,5 @@
+const B = () => {
+  return <div className="bg-blue-400 m-12 p-12">B</div>;
+};
+
+export default B;
